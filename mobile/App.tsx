1b86c630716908a6423655eb1946385f44854ee9
David Vilaça <vilacapdavid@gmail.com>
2020-03-26 18:48:22 -0300

    fix(mobile): iOS status bar dark content

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,14 @@
 import 'intl'
 import 'intl/locale-data/jsonp/pt-BR'
-import React from 'react'
+import React, { Fragment } from 'react'
+import { StatusBar } from 'react-native'
 import Routes from './src/routes'
 
 export default function App() {
-  return <Routes />
+  return (
+    <Fragment>
+      <StatusBar barStyle='dark-content' />
+      <Routes />
+    </Fragment>
+  )
 }
