9d945cc43ff78aff0a593f27f1c5a21163a3ef19
David Vilaça <vilacapdavid@gmail.com>
2020-03-27 20:57:49 -0300

    fix(api): general route 500 next handler

--- api/src/controllers/general.controller.ts
+++ api/src/controllers/general.controller.ts
@@ -3,7 +3,10 @@ import { InternalServerError } from 'http-errors'
 
 const ping = (_: Request, res: Response<string>) => res.json('Pong!')
 
-const e500 = (_: Request, __: Response<never>, next: NextFunction) =>
-  next(new InternalServerError())
+const e500 = (_: Request, __: Response<never>, next: NextFunction) => {
+  const err = new InternalServerError()
+  err.statusCode = null as any
+  next(err)
+}
 
 export { ping, e500 }
