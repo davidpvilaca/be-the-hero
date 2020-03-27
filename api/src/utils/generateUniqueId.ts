0895e93c5fff1de811829d105b9ccd63a59d9935
David Vilaça <vilacapdavid@gmail.com>
2020-03-27 10:14:50 -0300

    fix(api): generate unique id return

--- api/src/utils/generateUniqueId.ts
+++ api/src/utils/generateUniqueId.ts
@@ -1,7 +1,7 @@
 import * as crypto from 'crypto'
 
 const generateUniqueId = () => {
-  crypto.randomBytes(4).toString('HEX')
+  return crypto.randomBytes(4).toString('HEX')
 }
 
 export default generateUniqueId
