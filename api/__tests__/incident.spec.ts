477b459fbead69deca71d1d9503540783d809740
David Vilaça <vilacapdavid@gmail.com>
2020-03-27 20:35:34 -0300

    test(api): update incident tests

--- api/__tests__/incident.spec.ts
+++ api/__tests__/incident.spec.ts
@@ -1,15 +1,10 @@
 import * as request from 'supertest'
 import connection from '../src/db/connection'
-import { IIncident, IOng } from '../src/interfaces'
+import { IOng } from '../src/interfaces'
 import app from '../src/main'
 import { generateUniqueId } from '../src/utils'
 
 describe('INCIDENT', () => {
-  const INCIDENT1 = {
-    title: 'Caso 1',
-    description: 'Detalhe do caso',
-    value: 120
-  }
   const ONG: IOng = {
     id: generateUniqueId(),
     name: 'APAD',
@@ -17,6 +12,11 @@ describe('INCIDENT', () => {
     whatsapp: '27999999999',
     city: 'Rio do Sul',
     uf: 'SC'
+  }
+  const INCIDENT1 = {
+    title: 'Caso 1',
+    description: 'Detalhe do caso',
+    value: 120
   }
 
   beforeEach(async () => {
@@ -32,6 +32,9 @@ describe('INCIDENT', () => {
     await connection.destroy()
   })
 
+  /**
+   * Success cases
+   */
   it('should be able to create a new incident', async () => {
     const res = await request(app)
       .post('/incidents')
@@ -52,10 +55,7 @@ describe('INCIDENT', () => {
   })
 
   it('should be list all incidents with a element', async () => {
-    await request(app)
-      .post('/incidents')
-      .set('authorization', ONG.id)
-      .send(INCIDENT1)
+    await connection('incidents').insert({ ...INCIDENT1, ong_id: ONG.id })
     const res = await request(app)
       .get('/incidents')
       .set('Authorization', ONG.id)
@@ -66,15 +66,67 @@ describe('INCIDENT', () => {
   })
 
   it('should be able to delete a incident', async () => {
-    const { body: incidnetCreated }: { body: IIncident } = await request(app)
-      .post('/incidents')
-      .set('authorization', ONG.id)
-      .send(INCIDENT1)
+    const [id]: number[] = await connection('incidents').insert({
+      ...INCIDENT1,
+      ong_id: ONG.id
+    })
 
     const res = await request(app)
-      .delete(`/incidents/${incidnetCreated.id}`)
+      .delete(`/incidents/${id}`)
       .set('Authorization', ONG.id)
     expect(res.status).toBe(204)
     expect(JSON.stringify(res.body)).toMatch('{}')
+  })
+
+  /**
+   * Fail cases
+   */
+  it('should not be able to create a new incident without authorization', async () => {
+    const res = await request(app)
+      .post('/incidents')
+      .send(INCIDENT1)
+    expect(res.body).toHaveProperty('message')
+    expect(res.status).toBe(401)
+  })
+
+  it('should not be able to create a new incident with invalid body', async () => {
+    const res = await request(app)
+      .post('/incidents')
+      .send({})
+    expect(res.body).toHaveProperty('message')
+    expect(res.body).toHaveProperty('validation')
+    expect(res.status).toBe(400)
+  })
+
+  it('should not be able to delete a incident without authorization', async () => {
+    const [id]: number[] = await connection('incidents').insert({
+      ...INCIDENT1,
+      ong_id: ONG.id
+    })
+
+    const res = await request(app).delete(`/incidents/${id}`)
+    expect(res.status).toBe(401)
+    expect(res.body).toHaveProperty('message')
+  })
+
+  it('should not be able to delete a incident with a different ong id', async () => {
+    const [id]: number[] = await connection('incidents').insert({
+      ...INCIDENT1,
+      ong_id: ONG.id
+    })
+
+    const res = await request(app)
+      .delete(`/incidents/${id}`)
+      .set('Authorization', generateUniqueId())
+    expect(res.status).toBe(403)
+    expect(res.body).toHaveProperty('message')
+  })
+
+  it('should not be able to delete a incident with invalid id', async () => {
+    const res = await request(app)
+      .delete('/incidents/0')
+      .set('authorization', ONG.id)
+    expect(res.status).toBe(400)
+    expect(res.body).toHaveProperty('message')
   })
 })
